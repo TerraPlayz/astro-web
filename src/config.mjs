2a81ac033309f2e310b369f2d2525590a18e69e8
Omxpro <omsenjalia@gmail.com>
2022-08-22 12:59:22 +0530

    Update config.mjs

--- src/config.mjs
+++ src/config.mjs
@@ -1,6 +1,7 @@
 export const SITE = {
   name: "TerraPlayz",
   domain: "https://terraplayz.github.io",
+  base: "/astro-web",
   github: "https://github.com/terraplayz/astro-web",
 
   postsPerPage: 6,
